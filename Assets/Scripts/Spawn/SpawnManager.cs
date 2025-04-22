8e8d52fb152bfde8cd41a4ea741472ee76f7f846
Mate93934 <mateodjm@gmail.com>
2025-04-22 20:55:59 -0300

    Import cat model
    - Import cat mesh with its respective armature and texture

--- Assets/Scripts/Spawn/SpawnManager.cs
+++ Assets/Scripts/Spawn/SpawnManager.cs
@@ -29,11 +29,11 @@ public class SpawnManager : MonoBehaviour
             Debug.Log("<color=yellow>Executing...");
             // Wait until object is at his despawn position
             yield return new WaitUntil(() => currentGO.transform.position.z <= config.despawnAtZ);
-            // Returns object at the end of the Queue
+            // Returns object at the end of the Queue and SetActive(false)
             PoolManager.Instance.Return(config.poolID, currentGO);
             // Instantiates object at the end of scenario, using respawnAt reference from "config"
             PoolManager.Instance.Get(config);
-            // Takes next object to pool
+            // Takes next object from pool
             currentGO = PoolManager.Instance.PeekAt(config.poolID);
         }
     }
